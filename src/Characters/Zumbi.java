aabef74a3ad6a2eaea9c32ae622a1d5f1ee02dba
Samuel <samuel.ferracini@hotmail.com>
2019-10-01 22:11:02 -0300

    Logica do zumbi quase completa

--- src/Characters/Zumbi.java
+++ src/Characters/Zumbi.java
@@ -17,6 +17,7 @@ public class Zumbi extends Personagem {
     private static final int MOVER_DIREITA = 3;
     private static final int MOVER_ESQUERDA = 4;
     private static final int QUANTIDADE_MOVIMENTOS = 5;
+    private static int teste;
 
     public Zumbi(Ponto2D posicao) {
         super(posicao, SIMBOLO);
@@ -38,49 +39,84 @@ public class Zumbi extends Personagem {
     public void atualizar(Mundo mundo) {
 
         int somaZumbi = this.posicao.getX() + this.posicao.getY();
-        int diferenca = 1000000;
+        int diferenca = 10000;
         Entidade alvo = new Entidade();
 
+//        for (Entidade entidade : mundo.getEntidades()) {
+//            if (entidade.getSimbolo() != Zumbi.SIMBOLO && entidade.getSimbolo() != Tesouro.SIMBOLO && entidade.getSimbolo() != Chave.SIMBOLO && entidade.getSimbolo() != Portal.SIMBOLO) {
+//                if (diferenca > Math.abs(entidade.posicao.getX() + entidade.posicao.getY() - somaZumbi)) {
+//                    alvo = entidade;
+//                    diferenca = Math.abs(entidade.posicao.getX() + entidade.posicao.getY() - somaZumbi);
+//                }
+//            }
+//        }
+//if (diferenca > Math.abs(mundo.getJogador().posicao.getX() + mundo.getJogador().posicao.getY() - somaZumbi)) {
+//            alvo = mundo.getJogador();
+//            diferenca = Math.abs(mundo.getJogador().posicao.getX() + mundo.getJogador().posicao.getY() - somaZumbi);
+//        }
         for (Entidade entidade : mundo.getEntidades()) {
             if (entidade.getSimbolo() != Zumbi.SIMBOLO && entidade.getSimbolo() != Tesouro.SIMBOLO && entidade.getSimbolo() != Chave.SIMBOLO && entidade.getSimbolo() != Portal.SIMBOLO) {
-                if (diferenca > Math.abs(entidade.posicao.getX() + entidade.posicao.getY() - somaZumbi)) {
+                if (diferenca > (Math.abs(this.posicao.getX() - entidade.posicao.getX()) + Math.abs(this.posicao.getY() - entidade.posicao.getY()))) {
                     alvo = entidade;
-                    diferenca = Math.abs(entidade.posicao.getX() + entidade.posicao.getY() - somaZumbi);
+                    diferenca = (Math.abs(this.posicao.getX() - entidade.posicao.getX()) + Math.abs(this.posicao.getY() - entidade.posicao.getY()));
                 }
             }
         }
 
-        if (diferenca > Math.abs(mundo.getJogador().posicao.getX() + mundo.getJogador().posicao.getY() - somaZumbi)) {
+        if (diferenca > Math.abs(mundo.getJogador().posicao.getX() - this.posicao.getX()) + Math.abs(mundo.getJogador().posicao.getY() - this.posicao.getY())) {
             alvo = mundo.getJogador();
-            diferenca = Math.abs(mundo.getJogador().posicao.getX() + mundo.getJogador().posicao.getY() - somaZumbi);
+            diferenca = Math.abs(mundo.getJogador().posicao.getX() - this.posicao.getX()) + Math.abs(mundo.getJogador().posicao.getY() - this.posicao.getY());
+        }
+        int xHorizontal;
+        int yHorizontal;
+        int xVertical;
+        int yVertical;
+
+        if (this.posicao.getX() > alvo.posicao.getX()) {
+            // ESQUERDA
+            xHorizontal = -1;
+            yHorizontal = 0;
+        } else if (this.posicao.getX() < alvo.posicao.getX()) {
+            // DIREITA
+            xHorizontal = 1;
+            yHorizontal = 0;
+        }else{
+            xHorizontal = 0;
+            yHorizontal = 0;
+        }
+        if (this.posicao.getY() < alvo.posicao.getY()) {
+           // CIMA
+            xVertical = 0;
+            yVertical = 1;
+        } else if (this.posicao.getY() > alvo.posicao.getY()){
+            // BAIXO
+            xVertical = 0;
+            yVertical = -1;
+        }else{
+        xVertical = 0;
+            yVertical = 0;
         }
 
-        if (Math.abs(alvo.posicao.getX() - this.posicao.getX()) != 0) {
-            if (alvo.posicao.getX() < this.posicao.getX()) {
-                if (!mundo.bloqueado(this.posicao.getX() - 1, this.posicao.getY())) {
-                    mover(mundo, -1, 0);
-                    return;
-                }
+        if (this.posicao.getX() == alvo.posicao.getX()) {
+            if (!mundo.bloqueado(this.posicao.getX() + xVertical, this.posicao.getY() + yVertical)) {
+                mover(mundo, xVertical, yVertical);
             } else {
-                if (!mundo.bloqueado(this.posicao.getX() + 1, this.posicao.getY())) {
-                    mover(mundo, +1, 0);
-                    return;
+                if (!mundo.bloqueado(this.posicao.getX() + xHorizontal, this.posicao.getY() + yHorizontal)) {
+                    mover(mundo, xHorizontal, yHorizontal);
                 }
             }
-        }
-        if (Math.abs(alvo.posicao.getY() - this.posicao.getY()) != 0) {
-            if (alvo.posicao.getY() < this.posicao.getY()) {
-                if (!mundo.bloqueado(this.posicao.getX(), this.posicao.getY() - 1)) {
-                    mover(mundo, 0, -1);
-                    return;
-                }
+        } else {
+            if (!mundo.bloqueado(this.posicao.getX() + xHorizontal, this.posicao.getY() + yHorizontal)) {
+                mover(mundo, xHorizontal, yHorizontal);
             } else {
-                if (!mundo.bloqueado(this.posicao.getX(), this.posicao.getY() + 1)) {
-                    mover(mundo, 0, +1);
-                    return;
+                if (!mundo.bloqueado(this.posicao.getX() + xVertical, this.posicao.getY() + yVertical)) {
+                    mover(mundo, xVertical, yVertical);
                 }
+
             }
+
         }
+
     }
 
 }
